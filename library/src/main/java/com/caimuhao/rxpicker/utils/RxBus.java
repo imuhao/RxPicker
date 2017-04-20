a63cf4843a76ea2e9840bbde334dcec76529aa57
smile <imuhao@qq.com>
2017-04-20 19:30:19 +0800

    initialize project

--- library/src/main/java/com/caimuhao/rxpicker/utils/RxBus.java
+++ library/src/main/java/com/caimuhao/rxpicker/utils/RxBus.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2017 Drakeet <drakeet.me@gmail.com>
- *
- * This file is part of rebase-android
- *
- * rebase-android is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * rebase-android is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with rebase-android. If not, see <http://www.gnu.org/licenses/>.
- */
 
 package com.caimuhao.rxpicker.utils;
 
