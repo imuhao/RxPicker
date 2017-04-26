7b253bd974b4b88f655b31731f73a02b885036e0
smile <imuhao@qq.com>
2017-04-26 18:22:20 +0800

    code optimize

--- app/src/main/java/com/caimuhao/sample/MainActivity.java
+++ app/src/main/java/com/caimuhao/sample/MainActivity.java
@@ -17,8 +17,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
   private Toolbar toolbar;
-  private TextView btnSingleImg;
-  private TextView btnMultiImg;
+  private TextView tvSingleImg;
+  private TextView tvMultiImg;
 
   private RecyclerView recyclerView;
   private PickerAdapter adapter;
@@ -29,11 +29,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     toolbar = (Toolbar) findViewById(R.id.toolbar);
     toolbar.setTitle("RxPicker");
-    btnSingleImg = (TextView) findViewById(R.id.btn_single_img);
-    btnSingleImg.setOnClickListener(this);
+    tvSingleImg = (TextView) findViewById(R.id.btn_single_img);
+    tvSingleImg.setOnClickListener(this);
 
-    btnMultiImg = (TextView) findViewById(R.id.btn_multi_img);
-    btnMultiImg.setOnClickListener(this);
+    tvMultiImg = (TextView) findViewById(R.id.btn_multi_img);
+    tvMultiImg.setOnClickListener(this);
 
     adapter = new PickerAdapter();
     recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
@@ -43,13 +43,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   @Override public void onClick(View v) {
-    if (btnSingleImg == v) {
+    if (tvSingleImg == v) {
       RxPicker.of().start(this).subscribe(new Consumer<List<ImageItem>>() {
         @Override public void accept(@NonNull List<ImageItem> imageItems) throws Exception {
           adapter.setData(imageItems);
         }
       });
-    } else if (btnMultiImg == v) {
+    } else if (tvMultiImg == v) {
       RxPicker.of()
           .single(false)
           .camera(true)
